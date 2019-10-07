d095d73dba5dfdd2d915733ef360cf72b7cede2e
xiangrui2019 <xiangrui@aiursoft.com>
2019-10-07 01:20:50 -0700

    finish

--- dogego_module_dbseeder.go
+++ dogego_module_dbseeder.go
@@ -1,6 +1,9 @@
 package dogego_module_dbseeder
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"log"
+)
 
 type DBSeeder struct {
 	masterDB *gorm.DB
@@ -14,6 +17,23 @@ func NewSeeder(masterDB *gorm.DB, slaveDB *gorm.DB) *DBSeeder {
 	}
 }
 
-func (seeder *DBSeeder) Seed(seed_list []interface{}) {
+func (seeder *DBSeeder) Seed(table_name string, seed_list []interface{}) *DBSeeder {
+	for _, seed_data := range seed_list {
+		datas := make([]interface{}, 0)
+		err := seeder.slaveDB.Table(table_name).Where(&datas, seed_data).Error
 
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(datas) == 0 {
+			err := seeder.masterDB.Create(seed_data).Error
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	return seeder
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/doge-soft/dogego_module_dbseeder
 
 go 1.13
 
-require github.com/jinzhu/gorm v1.9.11 // indirect
+require github.com/jinzhu/gorm v1.9.11
